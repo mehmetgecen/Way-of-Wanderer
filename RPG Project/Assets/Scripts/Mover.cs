6e2429dcb13710465dc2b6a7cab1841d0e2c6a7a
mehmetgecen <mehmetgecen06@gmail.com>
2023-02-22 14:14:31 +0300

    Basic Locomotion Update
    Click to Move Mechanic Implemented. NavMesh Fixes Added.

--- RPG Project/Assets/Scripts/Mover.cs
+++ RPG Project/Assets/Scripts/Mover.cs
@@ -2,12 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UIElements;
 
 public class Mover : MonoBehaviour
 {
     [SerializeField] private Transform targetTransform;
-    
     private NavMeshAgent _playerNavMesh;
+    
     
     void Start()
     {
@@ -16,6 +17,24 @@ public class Mover : MonoBehaviour
     
     void Update()
     {
-        _playerNavMesh.SetDestination(targetTransform.position);
+        if (Input.GetMouseButtonDown(0))
+        {
+            MoveToCursor(); 
+        }
+    }
+
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        bool hasHit = Physics.Raycast(ray, out hit);
+
+        if (hasHit)
+        {
+            _playerNavMesh.SetDestination(hit.point);
+        }
+        
+        //Debug.DrawRay(ray.origin,ray.direction * 100); 
     }
 }
