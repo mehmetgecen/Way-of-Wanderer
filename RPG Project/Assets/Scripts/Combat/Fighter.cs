0ff38f142837a662d1684f0c9448a920ad43fc4d
mehmetgecen <mehmetgecen06@gmail.com>
2023-02-23 01:25:17 +0300

    Targeting System Implementation
    Enemy Recognition and Target based Raycasting Implemented.

---
+++ RPG Project/Assets/Scripts/Combat/Fighter.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Combat
+{
+    public class Fighter : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        public void Attack(CombatTarget target)
+        {
+            Debug.Log("Defend Yourself Peasant!");
+        }
+
+    }
+}
+
--- RPG Project/Assets/Scripts/Control/PlayerController.cs
+++ RPG Project/Assets/Scripts/Control/PlayerController.cs
@@ -2,16 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using RPG.Movement;
+using RPG.Combat;
+using UnityEngine.UIElements;
 
 namespace RPG.Control
 {
     public class PlayerController : MonoBehaviour
     {
-        public GameObject enemy;
+        void Update()
+        {
+            InteractWithMovement();
+            InteractWithCombat();
+        }
 
+        private void InteractWithCombat()
+        {
+            RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
+
+            foreach (RaycastHit hit in hits)
+            {
+                CombatTarget target = hit.transform.GetComponent<CombatTarget>();
+
+                if (target==null) continue;
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    GetComponent<Fighter>().Attack(target);
+                }
+            }
+        }
+        
+        private void InteractWithMovement()
+        {
+            if (Input.GetMouseButton(0))
+            {
+                MoveToCursor();
+            }
+        }
         private void MoveToCursor()
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Ray ray = GetMouseRay();
             RaycastHit hit;
 
             bool hasHit = Physics.Raycast(ray, out hit);
@@ -24,12 +54,11 @@ namespace RPG.Control
             //Debug.DrawRay(ray.origin,ray.direction * 100); 
         }
 
-        void Update()
+        private static Ray GetMouseRay()
         {
-            if (Input.GetMouseButton(0))
-            {
-                MoveToCursor();
-            }
+            return Camera.main.ScreenPointToRay(Input.mousePosition);
         }
+        
+        
     }
 }
