a53e56decbb084ceb1688131ba574f6880a5d00f
mehmetgecen <mehmetgecen06@gmail.com>
2023-03-02 23:38:42 +0300

    Cutscene Improvements

---
+++ RPG Project/Assets/Scripts/Cinematics/CinematicTrigger.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace RPG.Cinematics
+{
+    public class CinematicTrigger : MonoBehaviour
+    {
+        private bool _alreadyTriggered = false;
+        
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player") && !_alreadyTriggered)
+            {
+                _alreadyTriggered = true;
+                GetComponent<PlayableDirector>().Play();
+                
+                // GetComponent<BoxCollider>().isTrigger = false;
+            }
+            
+            
+        }
+    } 
+}
+
+
