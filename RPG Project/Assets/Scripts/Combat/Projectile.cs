c94804868e4efbab96dc4fa955803d2032beab76
mehmetgecen <mehmetgecen06@gmail.com>
2023-03-11 16:30:00 +0300

    Fireball Update
    Fireball Projectile added to preparation of future mage class.
    
    Impact effect after projectiles added.

--- RPG Project/Assets/Scripts/Combat/Projectile.cs
+++ RPG Project/Assets/Scripts/Combat/Projectile.cs
@@ -4,66 +4,72 @@ using System.Collections.Generic;
 using RPG.Core;
 using UnityEngine;
 
-public class Projectile : MonoBehaviour
+namespace RPG.Combat
 {
-    
-    [SerializeField] private float projectileSpeed;
-    [SerializeField] private bool isHoming;
-    [SerializeField] private GameObject hitEffect = null;
-    
-    
-    float _projectileDamage;
-    Health _target;
-
-    private void Start()
+    public class Projectile : MonoBehaviour
     {
-        transform.LookAt(GetAimPosition());    
-    }
-
-    void Update()
-    {
-        if (isHoming && !_target.IsDead())
+        
+        [SerializeField] private float projectileSpeed;
+        [SerializeField] private bool isHoming;
+        [SerializeField] private GameObject hitEffect = null;
+        [SerializeField] private float maxLifetime = 10f;
+            
+        
+        float _projectileDamage;
+        Health _target;
+    
+        private void Start()
         {
-            transform.LookAt(GetAimPosition()); 
+            transform.LookAt(GetAimPosition());    
+        }
+    
+        void Update()
+        {
+            if (isHoming && !_target.IsDead())
+            {
+                transform.LookAt(GetAimPosition()); 
+            }
+            
+            transform.Translate(Vector3.forward * projectileSpeed * Time.deltaTime);
+        }
+    
+        public void SetTarget(Health target,float damage)
+        {
+            _target = target;
+            _projectileDamage = damage;
+            
+            Destroy(gameObject,maxLifetime);
         }
         
-        transform.Translate(Vector3.forward * projectileSpeed * Time.deltaTime);
-    }
-
-    public void SetTarget(Health target,float damage)
-    {
-        _target = target;
-        _projectileDamage = damage;
-    }
+        private Vector3 GetAimPosition()
+        {
+            CapsuleCollider targetCapsule = _target.GetComponent<CapsuleCollider>();
     
-    private Vector3 GetAimPosition()
-    {
-        CapsuleCollider targetCapsule = _target.GetComponent<CapsuleCollider>();
-
-        if (targetCapsule == null)
-        {
-            return _target.gameObject.transform.position;
+            if (targetCapsule == null)
+            {
+                return _target.gameObject.transform.position;
+            }
+            return _target.gameObject.transform.position + (Vector3.up * targetCapsule.height / 2);
         }
-        return _target.gameObject.transform.position + (Vector3.up * targetCapsule.height / 2);
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.GetComponent<Health>() != _target) return;
-        if(_target.IsDead()) return;
-        
-        Destroy(gameObject);
-        other.gameObject.GetComponent<Health>().TakeDamage(_projectileDamage);
-
-        if (gameObject.name.Contains("Fireball"))
+    
+        private void OnTriggerEnter(Collider other)
         {
-            Instantiate(hitEffect, GetAimPosition(), Quaternion.identity);
+            if (other.GetComponent<Health>() != _target) return;
+            if(_target.IsDead()) return;
+
+            
+            Destroy(gameObject);
+            other.gameObject.GetComponent<Health>().TakeDamage(_projectileDamage);
+            projectileSpeed = 0;
+    
+            if (gameObject.name.Contains("Fireball")) // will be edited for performance issues
+            {
+                Instantiate(hitEffect, GetAimPosition(), Quaternion.identity);
+                
+            }
             
         }
         
-        
-        
-        
-        
     }
+
 }
